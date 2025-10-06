8ebcae640de2095c307045fb62290a04039832cb
Thblslvr <tolebasrudenko@yandex.ru>
2025-10-06 10:44:04 +0500

    med2

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  base: "./", // важно для GitHub Pages
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+  },
+});
